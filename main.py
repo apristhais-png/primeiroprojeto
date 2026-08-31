3641385a290f6dcbe1eefeebf359f1fa51c26a2f
Priscila <apristhais@gmail>
2026-08-31 16:05:42 -0300

    Permite cadastrar varias pessoas no CSV

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import csv
 import os
 
-nome = input("Qual é o seu nome? ")
-idade = input("Qual é a sua idade? ")
-profissao = input("Qual é a sua profissão? ")
-
 arquivo = "dados.csv"
-
 arquivo_existe = os.path.exists(arquivo)
 
 with open(arquivo, "a", newline="", encoding="utf-8-sig") as arquivo_csv:
@@ -15,6 +10,16 @@ with open(arquivo, "a", newline="", encoding="utf-8-sig") as arquivo_csv:
     if not arquivo_existe:
         escritor.writerow(["Nome", "Idade", "Profissão"])
 
-    escritor.writerow([nome, idade, profissao])
+    while True:
+        nome = input("Qual é o seu nome? ")
+        idade = input("Qual é a sua idade? ")
+        profissao = input("Qual é a sua profissão? ")
+
+        escritor.writerow([nome, idade, profissao])
+
+        continuar = input("\nDeseja cadastrar outra pessoa? (s/n): ")
+
+        if continuar.lower() != "s":
+            break
 
 print("\nDados salvos com sucesso no arquivo dados.csv!")
