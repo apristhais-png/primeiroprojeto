00defee277eaf4fe3687882d5ac7a75c5c16c8e5
Priscila <apristhais@gmail>
2026-08-31 15:53:04 -0300

    Adiciona coleta e armazenamento de dados em CSV

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import csv
+import os
+
 nome = input("Qual é o seu nome? ")
+idade = input("Qual é a sua idade? ")
+profissao = input("Qual é a sua profissão? ")
 
-print(f"Olá, {nome}! Bem-vinda ao meu primeiro projeto em Python.")
+arquivo = "dados.csv"
 
+arquivo_existe = os.path.exists(arquivo)
 
+with open(arquivo, "a", newline="", encoding="utf-8-sig") as arquivo_csv:
+    escritor = csv.writer(arquivo_csv, delimiter=";")
+
+    if not arquivo_existe:
+        escritor.writerow(["Nome", "Idade", "Profissão"])
+
+    escritor.writerow([nome, idade, profissao])
+
+print("\nDados salvos com sucesso no arquivo dados.csv!")
